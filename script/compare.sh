46b64791f49d604178c9b30e22900b54c5d16edb
Yanqiao (Joyce) Wang <91919427+jyanqiaowang@users.noreply.github.com>
2025-09-16 20:12:00 -0400

    Delete script/compare.sh

--- script/compare.sh
+++
@@ -1,9 +0,0 @@
-python compare_two_excel.py \
-  --file-a /mnt/sunlab-nas-1/CVAT/underlit_annotated_datset.xlsx \
-  --file-b /mnt/sunlab-nas-1/CVAT/merged_labels_2update_with_face_yolo.csv \
-  --key-col "Video Name" \
-  --decision-col "Decision" \
-  --ratio-col-a "FaceDetectRatio" \
-  --ratio-col-b "UnderlitRatio" \
-  --label-col "label" \
-  --out-dir /mnt/sunlab-nas-1/CVAT/compare_out
